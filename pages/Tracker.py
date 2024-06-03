de381a27eea1a0f846931d744c9f63b80bf9b534
Rahul Vijayrao Punewar <rvpunewar@gmail.com>
2024-06-03 11:03:46 +0000

    Added Table

--- pages/Tracker.py
+++ pages/Tracker.py
@@ -1,22 +1,33 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-from urllib.error import URLError
-from st_aggrid import AgGrid
+
+from st_aggrid import AgGrid,GridUpdateMode
+from st_aggrid import GridOptionsBuilder
 
 st.set_page_config(
-    page_title="Dataframe demo",
+    page_title="Tracker",
     page_icon="excel"
 )
 
-data = {'Date':['John','Anna','Peter'],
-        'Age':[28,35,45]
+def data_upload():
+    df=pd.read_csv("Test_data.csv")
+    return df
 
-       }
+df = data_upload()
+#st.dataframe(data=df)
+#st.info(len(df))
 
-df =pd.DataFrame(data)
+gd = GridOptionsBuilder.from_dataframe(df)
+gd.configure_pagination(enabled=True)
+gd.configure_default_column(editable=True,groupable=True)
 
-AgGrid(df)
+sel_mode = st.radio('selection Type', options = ['single','multiple'])
+gd.configure_selection(selection_mode=sel_mode,use_checkbox=True)
+gridoptions = gd.build()
+grid_Table = AgGrid(df,gridOptions=gridoptions,
+                    update_mode=GridUpdateMode.SELECTION_CHANGED,
+                    height=500,
+                    allow_unsafe_jscode=True)
 
-
-
+sel_row = grid_Table["selected_rows"]
+st.write(sel_row)
